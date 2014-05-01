3cb01570ce82a8212dfbda4c834dd894797c18ca
Prelang Builder <builder@prelang.com>
2014-05-01 00:14:58 +0000

    Generated scaffolding for model 'Category' via Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Mysite::Application.routes.draw do
+  resources :categories
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
